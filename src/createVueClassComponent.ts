fcfbf6b4c0c7c6c735b8d49a283f76ab0472afe2
苑朋飞 <yuanpengfei@yuanpengfeideMacBook-Pro-2.local>
2021-06-14 12:35:15 +0800

    modify readme and optimize code when create same name component to warn user

--- src/createVueClassComponent.ts
+++ src/createVueClassComponent.ts
@@ -18,8 +18,7 @@ function getTemplate(componentName: string) {
     return `<template>\n</template>
     \n<script lang="ts">
     \nimport { Vue, Component, Prop } from "vue-property-decorator";
-    \n@Component
-    \nexport default class ${componentName} extends Vue {}
+    \n@Component\nexport default class ${componentName} extends Vue {}
     \n</script>
     \n<style lang="less" scoped>
     \n</style>`;
@@ -31,31 +30,33 @@ function writeFile(content: Buffer | string, filePath: string, componentName: st
     fs.writeFileSync(_filePath, content);
 }
 
-function createComponent(uri: { path: string }, componentName: string) {
-    const content = getTemplate(componentName);
+async function createComponent(uri: vscode.Uri, componentName: string) {
+    const path = uri.path;
+    const arr = path.split('/');
 
-
-    const arr = uri.path.split('/');
-
-    if (fs.lstatSync(uri.path).isFile()) {
+    if (fs.lstatSync(path).isFile()) {
         arr.pop();
     }
 
     const filePath = arr.join('/');
+   
+    if (fs.existsSync(`${filePath}/${componentName}.vue`)) {
+        vscode.window.showWarningMessage(`You already have one ${componentName} component.`);
+        return;
+    }
 
 
-    writeFile(content, filePath, componentName);
+    writeFile(getTemplate(componentName), filePath, componentName);
 }
 
 module.exports = function (context: { subscriptions: any[]; }) {
-    context.subscriptions.push(vscode.commands.registerCommand('createvueclasscomponent.create', async (uri: { path: string }) => {
+    context.subscriptions.push(vscode.commands.registerCommand('createvueclasscomponent.create', async (uri: vscode.Uri) => {
         const componentName = await vscode.window.showInputBox({
             ignoreFocusOut: true,
             placeHolder: 'MyComponent',
             prompt: 'What is the name of the new component?',
         });
-        
+
         createComponent(uri, convertComponentName(componentName!));
     }));
-
 };
