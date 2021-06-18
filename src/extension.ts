3700e921809b44d1c6f0764b71a4fe35c493d415
pengfeiyuan <ypf9319@gmail.com>
2021-06-18 08:32:00 +0800

    check function added

--- src/extension.ts
+++ src/extension.ts
@@ -1,8 +1,9 @@
 import * as vscode from 'vscode';
-import createVueClassComponent from './createVueClassComponent';
+// import createVueClassComponent from './createVueClassComponent';
+import checkDestinationVueFilename from './checkDestinationVueFilename';
 
 export function activate(context: vscode.ExtensionContext) {
-	createVueClassComponent(context);
+	checkDestinationVueFilename(context);
 }
 
 export function deactivate() { }
